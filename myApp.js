4e137010b2519898f1af4fe6a2d726477b74e3db
oussama smiai <smiai.oussama@gmail.com>
2021-11-12 15:55:00 +0000

    helmet

--- myApp.js
+++ myApp.js
@@ -8,14 +8,7 @@ const ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts( {maxAge: ninetyDaysInSeconds, force: true} ))
 app.use(helmet.dnsPrefetchControl())
 app.use(helmet.noCache())
-app.use(helmet.contentSecurityPolicy({directives: {
-                                            defaultSrc: ["'self'"]
-                                            ,scriptSrc: ["'self'", 'trusted-cdn.com']}}))
-//comment
-
-
-
-
+app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] }} ))
 
 
 

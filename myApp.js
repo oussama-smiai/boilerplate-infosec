e8d9701a2cecd48418381008bcfc41efeaa40196
oussama smiai <smiai.oussama@gmail.com>
2021-11-12 15:21:24 +0000

    no cache

--- myApp.js
+++ myApp.js
@@ -6,6 +6,8 @@ app.use(helmet.noSniff())
 app.use(helmet.ieNoOpen())
 const ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts( {maxAge: ninetyDaysInSeconds, force: true} ))
+app.use(helmet.dnsPrefetchControl())
+app.use(helmet.noCache())
 
 
 

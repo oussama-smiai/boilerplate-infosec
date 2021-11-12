9f46932d17c3111ce506bd7d6d6ddd6d8108a6d3
oussama smiai <smiai.oussama@gmail.com>
2021-11-12 15:58:52 +0000

    descriptive

--- myApp.js
+++ myApp.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const app = express();
 var helmet = require('helmet');
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff())
-app.use(helmet.ieNoOpen())
-const ninetyDaysInSeconds = 90*24*60*60;
-app.use(helmet.hsts( {maxAge: ninetyDaysInSeconds, force: true} ))
-app.use(helmet.dnsPrefetchControl())
-app.use(helmet.noCache())
-app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] }} ))
-
-
-
-
-
-
-
-
-
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false     // disable
+}))
 
 
 
